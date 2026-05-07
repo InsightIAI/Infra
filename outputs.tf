62fb7d95db14d6ad496dde404cd91d53d3d46d4f
Ru-kko <verase2003.1@gmail.com>
2026-05-07 07:46:57 -0500

    fix: Add missing output variables for backend security groups and ECS configuration

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,14 @@ output "network_frontend_subnet_id" {
   value = module.network.frontend_subnet_id
 }
 
+output "network_backend_sg_id" {
+  value = module.network.backend_sg_id
+}
+
+output "network_vpclink_sg_id" {
+  value = module.network.vpclink_sg_id
+}
+
 output "backend_alb_arn" {
   value = module.fargate.alb_arn
 }
@@ -36,6 +44,14 @@ output "cognito_domain_url" {
 
 output "aurora_cluster_endpoint" {
   value = module.database.aurora_cluster_endpoint
+}
+
+output "ecs_cluster_id" {
+  value = module.fargate.ecs_cluster_id
+}
+
+output "ecs_task_execution_role_arn" {
+  value = module.fargate.ecs_task_execution_role_arn
 }
 
 output "backend_repository_url" {
